d3a95430122413cb7ff175cc4486a2d4e5663035
durranee <durranee@hotmail.com>
2018-03-21 23:45:42 +0000

    Airport class land() almost done

--- Airport_Challenge_CSharp/Program.cs
+++ Airport_Challenge_CSharp/Program.cs
@@ -7,12 +7,12 @@ namespace Airport_Challenge_CSharp
     public class Plane
     {
         int id;
-        string airport_id;
+        string airportName;
 
-        public Plane(int _id, string _airport_id = null)
+        public Plane(int _id, string _airportName = null)
         {
             id = _id;
-            airport_id = _airport_id;
+            airportName = _airportName;
         }
 
         public int getId()
@@ -20,25 +20,67 @@ namespace Airport_Challenge_CSharp
             return id;
         }
 
-        public string getAirportId(){
-            return airport_id;
+        public void setAirportName(string _airportName)
+        {
+            airportName = _airportName;
         }
 
+        public string getAirportName(){
+            return airportName;
+        }
     }
+
 
     public class Weather
     {
-        Boolean[] stormy;
+        bool[] stormy;
 
         public Weather(){
-            stormy = new Boolean[] { false, false, false, false, true };
+            stormy = new bool[] { false, false, false, false, true };
         }
 
-        public Boolean isItStormy(){
+        public bool isItStormy(){
             Random rnd = new Random();
             return stormy[rnd.Next(0, 5)];
         }
     }
+
+
+    public class Airport
+    {
+        public string name;
+        public int capacity;
+
+        public Plane[] hanger;
+        public Airport(string _name, int _capacity = 30)
+        {
+            capacity = _capacity;
+            hanger = new Plane[capacity];
+            name = _name;
+        }
+
+        public bool isFull()
+        {
+            return hanger.Length >= capacity;
+        }
+
+        public void land(Plane plane, bool stormy){
+            if (plane.getAirportName() != null)
+                throw new System.InvalidOperationException("Plane already landed at an airport.");
+            
+            if (isFull())
+                throw new System.InvalidOperationException("Error! Airport Full.");
+
+            if (stormy)
+                throw new System.InvalidOperationException("BAD WEATHER CONDITION! Cannot allow to land.");
+
+            //plane.setAirportName(name);
+            ////hanger.
+            
+        }
+
+    }
+
 
 
     class MainClass
