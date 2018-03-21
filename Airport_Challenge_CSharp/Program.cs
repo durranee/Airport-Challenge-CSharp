1e4c62fdd93e62614a00d649e3b9c05bc4470143
durranee <durranee@hotmail.com>
2018-03-21 23:08:46 +0000

    Plane class setup complete

--- Airport_Challenge_CSharp/Program.cs
+++ Airport_Challenge_CSharp/Program.cs
@@ -2,11 +2,36 @@
 
 namespace Airport_Challenge_CSharp
 {
+   
+
+    public class Plane
+    {
+        int id;
+        string airport_id;
+
+        public Plane(int _id, string _airport_id = null)
+        {
+            id = _id;
+            airport_id = _airport_id;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public string getAirportId(){
+            return airport_id;
+        }
+
+    }
+
     class MainClass
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+                
+
         }
     }
 }
